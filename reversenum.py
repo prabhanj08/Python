270a16c46211a999b0be2086aee94174359e8ff0
prabhanj08 <40606209+prabhanj08@users.noreply.github.com>
2021-07-16 21:42:32 +0530

    reverse num

---
+++ reversenum.py
@@ -0,0 +1,15 @@
+def rev(number):
+    revs_number = 0
+
+    # reverse the integer number using the while loop
+
+    while (number > 0):
+        # Logic
+        remainder = number % 10
+        revs_number = (revs_number * 10) + remainder
+        number = number // 10
+
+    return revs_number
+
+
+print(rev(12345))
