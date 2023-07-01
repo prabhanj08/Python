2677c9ae00fcfbb6de8453e5ca1d77710bca7bc3
prabhanj08 <40606209+prabhanj08@users.noreply.github.com>
2023-07-01 23:14:22 +0530

    Binary Search
    Given an array of integers nums which is sorted in ascending order, and an
    integer target, write a function to search target in nums. If target exists, then
    return its index. Otherwise, return -1.
    You must write an algorithm with O(log n) runtime complexity.

---
+++ DSA/binarySearch.py
@@ -0,0 +1,32 @@
+"""
+Given an array of integers nums which is sorted in ascending order, and an
+integer target, write a function to search target in nums. If target exists, then
+return its index. Otherwise, return -1.
+You must write an algorithm with O(log n) runtime complexity.
+Example 1:
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+
+"""
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
